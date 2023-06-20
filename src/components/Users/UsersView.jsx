37711dd859e03af3fbd8502ec0fe826ede9884c2
Daniel Rubio <rubio.krkmo@gmail.com>
2023-06-19 19:00:11 -0600

    Add Image to backgroud card ContactUs

--- src/components/Users/UsersView.jsx
+++ src/components/Users/UsersView.jsx
@@ -35,7 +35,7 @@ function ContactUsView() {
             <div
               key={user.id}
               className="flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row"
-                style={{backgroundImage:`url(/src/assets/backgroundKodigo.jpg)`,backgroundSize:'cover'}}
+                style={{backgroundImage:`url(/src/img/backgroundKodigo.jpg)`,backgroundSize:'cover'}}
             >
               <img
                 className="  h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
